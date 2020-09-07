06fa8a19bf322f0b95a86c22bd53b0096d339cc4
Sina <sina.stocker@tum.de>
2020-09-07 16:32:56 +0200

    DEV: Add script with helpertools

---
+++ MLinCRS/helpers.py
@@ -0,0 +1,21 @@
+import errno
+import os
+
+def mkdir_p(path):
+    """
+    Creates directory with parent directory as needed
+    (similar to 'mkdir -p ${path}').
+    Does not raise an error if directory exists
+    Inputs:
+    -------
+    path:   type(str)
+    """
+    try:
+        os.makedirs(path)
+    except OSError as err:
+        if err.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise err
+    return
+
